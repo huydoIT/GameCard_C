36fd668094851c7e5966fb0bdf6f0fc4fed1bdf4
Huy Đỗ <huydo93.it@gmail.com>
2020-04-10 16:20:44 +0900

    add

--- GameCard.cpp
+++ GameCard.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <windows.h>
+
 #define LINE "\n====================================================================\n"
 int dt[4][13] = { 0 };
 /**
@@ -11,12 +14,12 @@ void createData() {
 		for (int j = 0; j < 13; j++) {
 			dt[i][j] = j + 1;
 		}
-		for (int j = 0; j < 13; j++) {
-			int r = rand() % 12;
-			int temp = dt[i][j];
-			dt[i][j] = dt[i][r];
-			dt[i][r] = temp;
-		}
+		//for (int j = 0; j < 13; j++) {
+		//	int r = rand() % 12;
+		//	int temp = dt[i][j];
+		//	dt[i][j] = dt[i][r];
+		//	dt[i][r] = temp;
+		//}
 	}
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 13; j++) {
@@ -40,7 +43,7 @@ void displayData() {
 	}
 	printf(LINE);
 	for (int i = 0; i < 4; i++) {
-		printf("%d||", i);
+		printf("%d |", i);
 		for (int j = 0; j < 13; j++) {
 			printf("%3d |", dt[i][j]);
 		}
@@ -76,12 +79,12 @@ void display(int r1, int c1, int r2, int c2) {
 		printf("\n--------------------------------------------------------------------\n");
 	}
 	printf("\n");
-	//printf("\n============================== ******* ==============================\n");
 }
+
 int main()
 {
 	srand((unsigned)time(NULL));
-	int count = 6;
+	int count = 10;
 	int p1 = 0, p2 = 0;
 	int game = 0;
 	createData();
@@ -89,8 +92,7 @@ int main()
 	while (count > 0)
 	{
 		int r1, c1, r2, c2;
-		//printData(tb);
-		//printTable();
+		//system("cls");
 		display(-1, -1, -1, -1);
 		printf("%s", game == 0 ? "Player A\n" : "Player B\n");
 		printf("Input choose [1]: ");
@@ -106,11 +108,12 @@ int main()
 			printf("%d point\n", game == 0 ? p1 += 2 : p2 += 2);
 		}
 		else {
-			printf("Sorry!\n");
+			printf("Sorry!\n\n");
+			//Sleep(3000);
 			game == 0 ? game = 1 : game = 0;
 		}
 	}
 	printf("=== The End ===\n");
 	printf("%s won!!!\n\n", p1 > p2 ? "Player A" : "Player B");
-	getchar();
+	return 0;
 }
